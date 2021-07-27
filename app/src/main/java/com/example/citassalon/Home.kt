8deeb619ea153e4febfb66ca7a78f9a8df457568
Karensza <k25gonz@gmail.com>
2021-07-27 15:10:31 -0500

    agendarStaff& agendarConfirmacion

--- app/src/main/java/com/example/citassalon/Home.kt
+++ app/src/main/java/com/example/citassalon/Home.kt
@@ -1,10 +1,12 @@
 package com.example.citassalon
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 
 class Home : AppCompatActivity() {
     private lateinit var btnAgendar: Button
@@ -15,15 +17,17 @@ class Home : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+        Toast.makeText(this, "Entrando a la primera activida", Toast.LENGTH_SHORT).show()
 
         btnAgendar = findViewById(R.id.btnAgendar)
         text = findViewById(R.id.text)
-
         btnAgendar.setOnClickListener {
             btnAgendar.isEnabled = false
             text.visibility = View.INVISIBLE
 
             btnFloatingPerfil = findViewById(R.id.btnFloatingPerfil)
             btnFloatingList = findViewById(R.id.btnFloatingList)
+            val intent = Intent(this, AgendarSucursal::class.java)
+            startActivity(intent)
     }
     }}
