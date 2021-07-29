1b4f33e1d830058b691a1da59716c650f1bd1928
Orlandroid <lopez.orlando.gold9@gmail.com>
2021-07-29 00:47:27 -0500

    Add README

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# SalonDeBelleza
+# Skeduly
+
+Proyecto realizado por Guadalupe, Karen , Rebeca y Ailil  Estudiantes de Bedu , este proyecto es para el modulo de kotlin intermedio.
+
+El objetivo principal de la app es la gestión de citas enfocado a establecimientos de belleza, como barberías y estéticas. También se incluyen algunas otras funciones para brindarle al usuario un mejor servicio, tales como información adicional sobre el establecimiento (servicios, ubicación, sucursales, staff) y una pequeña sección de e-commerce con productos que el establecimiento tenga a la venta.
+
+
+
+<img src="screenshots/Flujos app.png">
--- app/src/main/java/com/example/citassalon/AgendarSucursal.kt
+++ app/src/main/java/com/example/citassalon/AgendarSucursal.kt
@@ -2,6 +2,8 @@ package com.example.citassalon
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.ListView
 import android.widget.Toast
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -9,9 +11,16 @@ import com.example.citassalon.adapters.AdaptadorAgendarSucursal
 
 
 class AgendarSucursal : AppCompatActivity() {
+
+    private lateinit var listViewSucursales: ListView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_agendar_sucursal)
-        Toast.makeText(this, "Entrando a la segunda activida", Toast.LENGTH_SHORT).show()
+        val data = arrayOf("Zahra Norte", "Zahra Centro", "Zahra Sur", "CDMX", "Polanco")
+        val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, data)
+        listViewSucursales = findViewById(R.id.list_view_agendar_sucursal)
+        listViewSucursales.adapter = adapter
     }
+
 }
