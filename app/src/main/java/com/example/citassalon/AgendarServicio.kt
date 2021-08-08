97884e5b7d788596a731699a0377e4ea00fc32d0
Karensza <k25gonz@gmail.com>
2021-08-08 14:52:07 -0500

    agendar_servicio

--- app/src/main/java/com/example/citassalon/AgendarServicio.kt
+++ app/src/main/java/com/example/citassalon/AgendarServicio.kt
@@ -8,12 +8,19 @@ import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import android.widget.Button
 import android.widget.ListView
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.RecyclerView
+import com.example.citassalon.adapters.AdaptadorAgendarServicio
+import com.example.citassalon.adapters.AdaptadorAgendarSucursal
+import com.example.citassalon.models.Servicio
 
 class AgendarServicio : Fragment() {
 
     private lateinit var buttonNext: Button
+    private lateinit var servicio: TextView
+    private lateinit var recyclerViewServicio: RecyclerView
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -22,9 +29,26 @@ class AgendarServicio : Fragment() {
     ): View? {
         val view = inflater.inflate(R.layout.fragment_agendar_servicio, container, false)
         buttonNext = view.findViewById(R.id.button_next_servicio)
+        recyclerViewServicio = view.findViewById(R.id.recycler_agendar_servicio)
+        recyclerViewServicio.adapter =
+            AdaptadorAgendarServicio(
+                arrayListOf(
+                    Servicio("Corte de cabello"),
+                    Servicio("Aplicación de tinte"),
+                    Servicio("Tratamiento Capilar"),
+                    Servicio("Depilación"),
+                    Servicio("Alaciado"),
+                    Servicio("Corte de uñas"),
+                    Servicio("Limpieza facial"),
+                    Servicio("Maquillaje"),
+                    Servicio("Manicure"),
+                    Servicio("Pedicure")
+                ))
         buttonNext.setOnClickListener {
             findNavController().navigate(R.id.action_agendarServicio_to_agendarFecha)
         }
         return view
     }
+
+
 }
---
+++ app/src/main/java/com/example/citassalon/models/Servicio.kt
@@ -0,0 +1,3 @@
+package com.example.citassalon.models
+
+data class Servicio(var servicio: String)
