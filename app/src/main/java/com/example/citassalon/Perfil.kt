12939774ab0c6e0e34b0fffb2147fac3bfc352fc
Orlandroid <lopez.orlando.gold9@gmail.com>
2021-08-11 21:54:26 -0500

    Agregado navegacion a pantalla fecha

--- app/src/main/java/com/example/citassalon/Perfil.kt
+++ app/src/main/java/com/example/citassalon/Perfil.kt
@@ -1,9 +1,7 @@
 package com.example.citassalon
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import android.widget.ImageView
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
@@ -11,6 +9,16 @@ import androidx.navigation.fragment.findNavController
 class Perfil : Fragment() {
 
     private lateinit var imageButton: ImageView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.toolbar_menu, menu)
+        super.onCreateOptionsMenu(menu, inflater)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -24,4 +32,6 @@ class Perfil : Fragment() {
         }
         return view
     }
+
+
 }
