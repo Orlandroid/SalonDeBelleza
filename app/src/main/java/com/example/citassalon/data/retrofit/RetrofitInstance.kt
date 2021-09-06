95c3e0e4829d20e2614b6ee199d96ca318df2ec6
Orlandroid <lopez.orlando.gold9@gmail.com>
2021-09-05 23:35:52 -0500

    Add api of country and put data on recyclerSucursal

--- app/src/main/java/com/example/citassalon/data/retrofit/RetrofitInstance.kt
+++ app/src/main/java/com/example/citassalon/data/retrofit/RetrofitInstance.kt
@@ -1,4 +1,18 @@
 package com.example.citassalon.data.retrofit
 
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
 class RetrofitInstance {
+
+    companion object {
+
+
+        fun getRetrofit(): Retrofit {
+            return Retrofit.Builder()
+                .baseUrl("https://restcountries.eu/rest/v2/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+        }
+    }
 }
