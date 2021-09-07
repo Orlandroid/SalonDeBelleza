fe7875f9eaf2fbcbf178e4681cb3369b347de297
Orlandroid <lopez.orlando.gold9@gmail.com>
2021-09-07 00:15:26 -0500

    Add sensor

--- app/src/main/java/com/example/citassalon/fragments/AgendarStaff.kt
+++ app/src/main/java/com/example/citassalon/fragments/AgendarStaff.kt
@@ -1,26 +1,35 @@
 package com.example.citassalon.fragments
 
 
+import android.content.Context.SENSOR_SERVICE
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.content.ContextCompat.getSystemService
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
-import androidx.recyclerview.widget.GridLayoutManager
 import com.example.citassalon.R
 import com.example.citassalon.adapters.AdaptadorAgendarStaff
 import com.example.citassalon.databinding.FragmentAgendarStaffBinding
 import com.example.citassalon.models.Staff
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
-class AgendarStaff : Fragment(), BottomNavigationView.OnNavigationItemSelectedListener {
+class AgendarStaff : Fragment(), BottomNavigationView.OnNavigationItemSelectedListener,
+    SensorEventListener {
 
 
     private lateinit var staff: List<Staff>
     private var _binding: FragmentAgendarStaffBinding? = null
     private val binding get() = _binding!!
+
+    private lateinit var sensorManager: SensorManager
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -29,6 +38,7 @@ class AgendarStaff : Fragment(), BottomNavigationView.OnNavigationItemSelectedLi
     ): View? {
         _binding = FragmentAgendarStaffBinding.inflate(layoutInflater, container, false)
         binding.staffBottomNavigationView.setOnNavigationItemSelectedListener(this)
+        setUpSensorStuff()
         binding.recyclerStaff.setHasFixedSize(true)
         populateRecyclerView()
         return binding.root
@@ -60,6 +70,32 @@ class AgendarStaff : Fragment(), BottomNavigationView.OnNavigationItemSelectedLi
             }
             else -> false
         }
+    }
+
+    private fun setUpSensorStuff() {
+        // Create the sensor manager
+        sensorManager = requireContext().getSystemService(SENSOR_SERVICE) as SensorManager
+
+        // Specify the sensor you want to listen to
+        sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)?.also { accelerometer ->
+            sensorManager.registerListener(
+                this,
+                accelerometer,
+                SensorManager.SENSOR_DELAY_FASTEST,
+                SensorManager.SENSOR_DELAY_FASTEST
+            )
+        }
+    }
+
+
+    override fun onSensorChanged(event: SensorEvent?) {
+        if (event?.sensor?.type == Sensor.TYPE_ACCELEROMETER) {
+            Toast.makeText(requireContext(), event.sensor.name, Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
+        return
     }
 
 }
