f75c7a5ead1b900c0cfc2dc6349aefa3a704e7ab
Orlandroid <lopez.orlando.gold9@gmail.com>
2021-11-13 16:17:16 -0600

    Update UI of login to allow to the user login with google

--- app/src/main/java/com/example/citassalon/ui/login/Login.kt
+++ app/src/main/java/com/example/citassalon/ui/login/Login.kt
@@ -6,13 +6,11 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.animation.LinearInterpolator
-import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.example.citassalon.databinding.FragmentLoginBinding
 import com.example.citassalon.util.*
 import dagger.hilt.android.AndroidEntryPoint
-import java.util.*
 
 
 @AndroidEntryPoint
