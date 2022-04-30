3587e13466988a2eb02bf0cc812781342dea988c
Orlandroid <lopez.orlando.gold9@gmail.com>
2022-04-29 23:36:50 -0500

    Se agrega clase externa para notificaciones

--- app/src/main/java/com/example/citassalon/main/NotificationHelper.kt
+++ app/src/main/java/com/example/citassalon/main/NotificationHelper.kt
@@ -1,8 +1,56 @@
 package com.example.citassalon.main
 
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.content.Context.NOTIFICATION_SERVICE
+import android.graphics.Color
+import android.os.Build
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+import androidx.core.content.ContextCompat.getSystemService
+import com.example.citassalon.R
+import java.util.*
 
-class NotificationHelper {
 
+class NotificationHelper(
+    private val context: Context
+) {
+
+    init {
+        createNotificationChannel()
+    }
+
+
+    private fun createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val name = "My Channel"
+            val descriptionText = "Channel Description"
+            val importance = NotificationManager.IMPORTANCE_DEFAULT
+            val channel = NotificationChannel("MY_CHANNEL_ID", name, importance).apply {
+                description = descriptionText
+            }
+            val notificationManager: NotificationManager =
+                context.getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
+        }
+    }
+
+    fun sendNotification(
+        title: String,
+        contentText: String,
+    ) {
+        val builder = NotificationCompat.Builder(context, "MY_CHANNEL_ID")
+            .setSmallIcon(R.drawable.watch_later_24px)
+            .setContentText(contentText)
+            .setSubText(title)
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+        with(NotificationManagerCompat.from(context)) {
+            notify(1, builder.build())
+        }
+    }
 
 }
+
 
--- app/src/main/java/com/example/citassalon/ui/cita_agendada/CitaAgendadaFragment.kt
+++ app/src/main/java/com/example/citassalon/ui/cita_agendada/CitaAgendadaFragment.kt
@@ -1,29 +1,22 @@
 package com.example.citassalon.ui.cita_agendada
 
 import android.animation.Animator
-import android.app.NotificationChannel
-import android.app.NotificationManager
-import android.content.Context
-import android.os.Build
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.annotation.RequiresApi
-import androidx.core.app.NotificationCompat
-import androidx.core.app.NotificationManagerCompat
 import androidx.navigation.fragment.findNavController
 import com.example.citassalon.R
 import com.example.citassalon.databinding.FragmentCitaAgendadaBinding
-
+import com.example.citassalon.main.NotificationHelper
 
 
 class CitaAgendadaFragment : Fragment(), Animator.AnimatorListener {
 
     private var _binding: FragmentCitaAgendadaBinding? = null
     private val binding get() = _binding!!
-    private val CHANNEL_ID = "hola"
+    private var notificationHelper: NotificationHelper? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -35,13 +28,14 @@ class CitaAgendadaFragment : Fragment(), Animator.AnimatorListener {
     }
 
     private fun setUpUi() {
+        notificationHelper = NotificationHelper(requireContext())
+        notificationHelper!!.sendNotification(
+            getString(R.string.cita_agendada),
+            "Cita"
+        )
         loadAnimation()
         binding.buttonAceptar.setOnClickListener {
-            findNavController().popBackStack(R.id.home3,false)
-        }
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            setNotificationChannel()
-            simpleNotification()
+            findNavController().popBackStack(R.id.home3, false)
         }
     }
 
@@ -54,37 +48,6 @@ class CitaAgendadaFragment : Fragment(), Animator.AnimatorListener {
         return this
     }
 
-    @RequiresApi(Build.VERSION_CODES.O)
-    private fun setNotificationChannel() {
-        val name = getString(R.string.Descuento)
-        val descriptionText = getString(R.string.date_selected)
-        val importance = NotificationManager.IMPORTANCE_DEFAULT
-        val channel = NotificationChannel(CHANNEL_ID, name, importance).apply {
-            description = descriptionText
-        }
-
-        val notificationManager: NotificationManager =
-            requireContext().getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-
-        notificationManager.createNotificationChannel(channel)
-    }
-
-    @RequiresApi(Build.VERSION_CODES.M)
-    private fun simpleNotification() {
-        var builder = context?.let {
-            NotificationCompat.Builder(requireContext(), CHANNEL_ID)
-                .setSmallIcon(R.drawable.watch_later_24px) //seteamos el ícono de la push notification
-                .setColor(it.getColor(R.color.beige)) //definimos el color del ícono y el título de la notificación
-                .setContentTitle(getString(R.string.cita_agendada)) //seteamos el título de la notificación
-                .setContentText(getString(R.string.notificacion_mensaje)) //seteamos el cuerpo de la notificación
-                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
-        } //Ponemos una prioridad por defecto
-
-        //lanzamos la notificación
-        with(NotificationManagerCompat.from(requireContext())) {
-            builder?.let { notify(20, it.build()) } //en este caso pusimos un id genérico
-        }
-    }
 
     override fun onDestroy() {
         super.onDestroy()
