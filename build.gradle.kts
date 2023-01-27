ad5ccdf9d9c20305f8d8cdbe9576657431b1a395
Orlandroid <lopez.orlando.gold9@gmail.com>
2023-01-26 21:25:28 -0600

    Change file to .kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,17 +1,15 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 buildscript {
-    //ext.kotlin_version = "1.5.21"
-    //ext.compose_version = '1.0.1'
     repositories {
         google()
         mavenCentral()
     }
     dependencies {
         classpath("com.android.tools.build:gradle:4.2.2")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.31")
-        classpath("com.google.gms:google-services:4.3.10")
-        val nav_version = "2.3.5"
-        classpath("androidx.navigation:navigation-safe-args-gradle-plugin:$nav_version")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.0")
+        classpath("com.google.gms:google-services:4.3.15")
+        val navVersion = "2.3.5"
+        classpath("androidx.navigation:navigation-safe-args-gradle-plugin:$navVersion")
         classpath("com.google.dagger:hilt-android-gradle-plugin:2.44")
 
         // NOTE: Do not place your application dependencies here; they belong
