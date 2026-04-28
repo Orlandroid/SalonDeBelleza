8792d86eb579700fe2342b2b1f394d054cfab6e8
Orlandroid <lopez.orlando.gold9@gmail.com>
2026-04-28 17:04:57 -0600

    updated compile sdk version and min sdk version

--- buildSrc/src/main/kotlin/ConfigData.kt
+++ buildSrc/src/main/kotlin/ConfigData.kt
@@ -1,8 +1,8 @@
 package com.example.androidbase.presentation
 
 object ConfigData {
-    const val COMPILE_SDK_VERSION = 34
-    const val MIN_SDK_VERSION = 22
+    const val COMPILE_SDK_VERSION = 35
+    const val MIN_SDK_VERSION = 23
     const val TARGET_SDK_VERSION = 33
     const val TEST_INSTRUMENTATION_RUNNER = "androidx.test.runner.AndroidJUnitRunner"
 }
