7aff1724da365041eee824cc896af59fa93e6a10
Orlandroid <lopez.orlando.gold9@gmail.com>
2026-04-29 15:26:38 -0600

    Migration Gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,3 +27,13 @@ allprojects {
 tasks.register("clean", Delete::class) {
     delete(rootProject.buildDir)
 }
+
+//plugins {
+//    alias(libs.plugins.android.application) apply false
+//    alias(libs.plugins.kotlin.android) apply false
+//    alias(libs.plugins.kotlin.compose) apply false
+//    alias(libs.plugins.navigation.safe.gradle) apply false
+//    alias(libs.plugins.hilt.android.gradle) apply false
+//    alias(libs.plugins.jetbrains.kotlin.jvm) apply false
+//    alias(libs.plugins.android.library) apply false
+//}
