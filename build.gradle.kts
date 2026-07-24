c4dca14a91b50e3b75a1c3692391172f1cf8b7a8
Orlandroid <lopez.orlando.gold9@gmail.com>
2026-07-24 10:30:48 -0600

    update APG

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ buildscript {
     }
     dependencies {
         val kotlinVersion = "2.2.21"
-        classpath("com.android.tools.build:gradle:9.3.0")
+        classpath("com.android.tools.build:gradle:9.3.1")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion")
         classpath("com.google.gms:google-services:4.4.4")
         classpath("com.google.dagger:hilt-android-gradle-plugin:2.57.2")
