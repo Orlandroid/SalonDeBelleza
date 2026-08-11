1de7892e9eca03635cab27c810ec7bb81f300920
Orlandroid <lopez.orlando.gold9@gmail.com>
2026-08-11 10:09:02 -0600

    Added profile module

---
+++ profile/src/main/java/com/example/profile/ProfileMenuProvider.kt
@@ -0,0 +1,28 @@
+package com.example.profile
+
+import com.example.domain.perfil.MENU
+import com.example.domain.perfil.ProfileItem
+
+object ProfileMenuProvider {
+
+    fun getMenusProfile(): List<ProfileItem> {
+        val elementsMenu = arrayListOf<ProfileItem>()
+        val profile = ProfileItem(R.string.profile, R.drawable.perfil, MENU.PROFILE)
+        val history =
+            ProfileItem(R.string.appointment_history, R.drawable.historial_menu, MENU.HISTORY)
+        val contactUs = ProfileItem(R.string.contact_us, R.drawable.contactos, MENU.CONTACTS)
+        val terminos =
+            ProfileItem(
+                R.string.terms_and_conditions,
+                R.drawable.terminos,
+                MENU.TERMS_AND_CONDITIONS
+            )
+        val closeSession = ProfileItem(R.string.logout, R.drawable.cerrar, MENU.CLOSE_SESSION)
+        elementsMenu.add(profile)
+        elementsMenu.add(history)
+        elementsMenu.add(contactUs)
+        elementsMenu.add(terminos)
+        elementsMenu.add(closeSession)
+        return elementsMenu
+    }
+}
